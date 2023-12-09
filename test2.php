e37f5346f7e44870c9b041f1b65316be909f4877
jhon202107786 <202107786@est.umss.edu>
2023-12-09 10:41:46 -0400

    t

--- test2.php
+++ test2.php
@@ -4,29 +4,33 @@ $username = "jhon";
 $password = "jhon";
 $database = "crmProyect";
 
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $database);
+try {
+    // Crear conexión
+    $conn = new mysqli($servername, $username, $password, $database);
 
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+    // Verificar la conexión
+    if ($conn->connect_error) {
+        throw new Exception("Conexión fallida: " . $conn->connect_error);
+    }
+
+    // Datos de prueba
+    $nombre = "John";
+    $apellido = "Doe";
+    $username = "john_doe";
+    $password = password_hash("secure_password", PASSWORD_DEFAULT);
+
+    // Consulta de inserción
+    $sql = "INSERT INTO usuarios (nombre, apellido, username, password) VALUES ('$nombre', '$apellido', '$username', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registro insertado correctamente";
+    } else {
+        throw new Exception("Error al insertar el registro: " . $conn->error);
+    }
+
+    // Cerrar la conexión
+    $conn->close();
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
 }
-
-// Datos de prueba
-$nombre = "John";
-$apellido = "Doe";
-$username = "john_doe";
-$password = password_hash("secure_password", PASSWORD_DEFAULT);
-
-// Consulta de inserción
-$sql = "INSERT INTO usuarios (nombre, apellido, username, password) VALUES ('$nombre', '$apellido', '$username', '$password')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Registro insertado correctamente";
-} else {
-    echo "Error al insertar el registro: " . $conn->error;
-}
-
-// Cerrar la conexión
-$conn->close();
 ?>
