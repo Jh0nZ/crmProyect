959b89b6b25b74ea3dd2dc51538460f35d81f017
jhon202107786 <202107786@est.umss.edu>
2023-12-09 12:30:04 -0400

    ff3

--- updateGit.php
+++ updateGit.php
@@ -4,7 +4,7 @@ chdir("/var/www/html/adrmrk.site/public_html");
 // Set the path to your private key
 
 // Execute the command with the key path
-exec("git pull git@github.com:Jh0nZ/crmProyect.git main 2>&1", $output);
+exec("git pull https://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@github.com/Jh0nZ/crmProyect.git main 2>&1", $output);
 // Muestra la salida del comando
 echo "<pre>" . implode("\n", $output) . "</pre>";
 ?>
