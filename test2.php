d1e156dabb6afc441489f7be77bce34374314632
jhon202107786 <202107786@est.umss.edu>
2023-12-09 10:33:16 -0400

    t

--- test2.php
+++ test2.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Guardar datos en una base de datos con POST</title>
-</head>
-<body>
-
 <?php
 // Configuración de la conexión a la base de datos
 $servername = "localhost";
@@ -14,16 +5,17 @@ $username = "jhon";
 $password = "jhon";
 $database = "crmProyect";
 
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $database);
 
-// Comprobar la conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
 
 // Comprobar si hay datos POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST)) {
+    // Crear conexión
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Comprobar la conexión
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
     // Obtener los datos del formulario
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
@@ -39,25 +31,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST)) {
     } else {
         echo 'Error al guardar datos: ' . $conn->error;
     }
+    $conn->close();
 }
 ?>
 
-<!-- Formulario de prueba con método POST -->
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" id="nombre" required>
-    <br>
-    <label for="apellido">Apellido:</label>
-    <input type="text" name="apellido" id="apellido" required>
-    <br>
-    <label for="username">Nombre de usuario:</label>
-    <input type="text" name="username" id="username" required>
-    <br>
-    <label for="password">Contraseña:</label>
-    <input type="password" name="password" id="password" required>
-    <br>
-    <button type="submit">Enviar</button>
-</form>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guardar datos en una base de datos con POST</title>
+</head>
+
+<body>
+
+
+    <!-- Formulario de prueba con método POST -->
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" id="nombre" required>
+        <br>
+        <label for="apellido">Apellido:</label>
+        <input type="text" name="apellido" id="apellido" required>
+        <br>
+        <label for="username">Nombre de usuario:</label>
+        <input type="text" name="username" id="username" required>
+        <br>
+        <label for="password">Contraseña:</label>
+        <input type="password" name="password" id="password" required>
+        <br>
+        <button type="submit">Enviar</button>
+    </form>
 
 </body>
+
 </html>
