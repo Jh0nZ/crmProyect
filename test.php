f90483f7957d189b0c35f07619f87937a96c03d2
jhon202107786 <202107786@est.umss.edu>
2023-12-09 10:07:33 -0400

    register

--- test.php
+++ test.php
@@ -4,31 +4,60 @@ $username = "jhon";
 $password = "jhon";
 $database = "crmProyect";
 
-// Crear conexión
-$conn = new mysqli($server, $username, $password, $database);
+// Verificar si se envió el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Crear conexión
+    $conn = new mysqli($server, $username, $password, $database);
 
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-}
-
-echo "Conexión exitosa";
-
-// Realizar una consulta simple
-$query = "SELECT * FROM tu_tabla";
-$result = $conn->query($query);
-
-// Verificar si la consulta fue exitosa
-if ($result) {
-    // Procesar los resultados
-    while ($row = $result->fetch_assoc()) {
-        echo "ID: " . $row["id"] . " - Nombre: " . $row["nombre"] . "<br>";
+    // Verificar la conexión
+    if ($conn->connect_error) {
+        die("Error de conexión: " . $conn->connect_error);
     }
-} else {
-    // Mostrar un mensaje de error si la consulta falla
-    echo "Error en la consulta: " . $conn->error;
-}
 
-// Cerrar la conexión
-$conn->close();
+    // Obtener datos del formulario
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash de la contraseña
+
+    // Preparar la consulta SQL
+    $query = "INSERT INTO usuarios (nombre, apellido, username, password) VALUES ('$nombre', '$apellido', '$username', '$password')";
+
+    // Ejecutar la consulta
+    if ($conn->query($query) === TRUE) {
+        echo "Registro exitoso";
+    } else {
+        echo "Error en el registro: " . $conn->error;
+    }
+
+    // Cerrar la conexión
+    $conn->close();
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de Usuario</title>
+</head>
+<body>
+    <h2>Registro de Usuario</h2>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" required><br>
+
+        <label for="apellido">Apellido:</label>
+        <input type="text" name="apellido" required><br>
+
+        <label for="username">Nombre de Usuario:</label>
+        <input type="text" name="username" required><br>
+
+        <label for="password">Contraseña:</label>
+        <input type="password" name="password" required><br>
+
+        <input type="submit" value="Registrar">
+    </form>
+</body>
+</html>
