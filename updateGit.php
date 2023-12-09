470867ffcb734f12631e8604548c0f50060b4a17
jhon202107786 <202107786@est.umss.edu>
2023-12-09 11:26:11 -0400

    up

--- updateGit.php
+++ updateGit.php
@@ -4,13 +4,14 @@
 $repo_dir = '/var/www/html/adrmrk.site/public_html';
 
 // Ejecuta el comando git pull
-$output = shell_exec("cd $repo_dir && git pull origin main");
+exec("cd $repo_dir && git pull origin main > git_log.txt 2>&1", $output, $return_code);
 
-// Verifica si el comando se ejecutó correctamente
-if ($output === null) {
-    echo "Error al ejecutar el comando.";
+
+// Verifica el código de salida del comando
+if ($return_code !== 0) {
+    echo "Error al ejecutar el comando. Código de salida: $return_code";
 } else {
-    echo "Comando ejecutado correctamente. Salida: $output";
+    echo "Comando ejecutado correctamente. Salida: " . implode("\n", $output);
 }
 
 echo "testeando";
