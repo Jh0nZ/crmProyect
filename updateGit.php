83950a1824149bc491a938f69d09ff5b1996bf21
jhon202107786 <202107786@est.umss.edu>
2023-12-09 12:23:02 -0400

    asd

--- updateGit.php
+++ updateGit.php
@@ -2,7 +2,6 @@
 // Cambia el directorio al del repositorio
 chdir("/var/www/html/adrmrk.site/public_html");
 // Set the path to your private key
-$keyPath = "/root/.ssh/id_ed25519.pub";
 
 // Execute the command with the key path
 exec("git pull https://jhon202107786:xqn4Nz66f_hE5HS@bitbucket.org/Jh0nZ/crmProyect.git main 2>&1", $output);
