3ea927d3bc9233594b6d6b6a4d86b8c5f50ddf2e
jhon202107786 <202107786@est.umss.edu>
2023-12-09 12:08:03 -0400

    aaa

--- updateGit.php
+++ updateGit.php
@@ -2,7 +2,7 @@
 // Cambia el directorio al del repositorio
 chdir("/var/www/html/adrmrk.site/public_html");
 // Set the path to your private key
-$keyPath = "/root/.ssh/id_ed25519";
+$keyPath = "/root/.ssh/id_ed25519.pub";
 
 // Execute the command with the key path
 exec("git pull origin main $keyPath 2>&1", $output);
