38efa1d9e08732f04c0e11e3ae61fc77722b7d07
jhon202107786 <202107786@est.umss.edu>
2023-12-09 00:28:45 -0400

    fix

--- updateGit.php
+++ updateGit.php
@@ -1,4 +1,7 @@
 <?php
-$output = shell_exec("git pull");
+$output = shell_exec("cd /var/www/html/adrmrk.site/public_html");
 echo "<pre>$output</pre>";
+$output = shell_exec("git pull origin main");
+echo "<pre>$output</pre>";
+
 ?>
