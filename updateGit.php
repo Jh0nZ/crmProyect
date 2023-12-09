080eda5b5c56f8d6446536df533af4dbb27be411
jhon202107786 <202107786@est.umss.edu>
2023-12-09 12:05:58 -0400

    ff

--- updateGit.php
+++ updateGit.php
@@ -1,8 +1,11 @@
 <?php
 // Cambia el directorio al del repositorio
 chdir("/var/www/html/adrmrk.site/public_html");
-// Ejecuta el comando git pull origin main
-exec("git pull origin main 2>&1", $output);
+// Set the path to your private key
+$keyPath = "/root/.ssh/id_ed25519.pub";
+
+// Execute the command with the key path
+exec("git pull origin main -i $keyPath 2>&1", $output);
 // Muestra la salida del comando
 echo "<pre>" . implode("\n", $output) . "</pre>";
 ?>
