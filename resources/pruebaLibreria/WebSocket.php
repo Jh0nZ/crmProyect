1425545059c433ac9bf724b9e8c676909ef30a14
jhon202107786 <202107786@est.umss.edu>
2023-12-09 22:55:17 -0400

    websocket

---
+++ resources/pruebaLibreria/WebSocket.php
@@ -0,0 +1,42 @@
+<?php
+namespace MyApp;
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class WebSocket implements MessageComponentInterface
+{
+    protected $clients;
+
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $this->clients->attach($conn);
+        echo "Nuevo cliente conectado ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        $this->clients->detach($conn);
+        echo "Cliente desconectado ({$conn->resourceId})\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        echo "Error: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
