673e512daaa9921f3742e15e1bbfa0a26cbe1c89
jhon202107786 <202107786@est.umss.edu>
2023-12-09 21:37:50 -0400

    tests

--- test.php
+++ test.php
@@ -1,63 +1,17 @@
 <?php
-$server = "localhost";
-$username = "jhon";
-$password = "jhon";
-$database = "crmProyect";
+// Check if the request method is POST
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Get the POST data
+    $name = isset($_POST["name"]) ? $_POST["name"] : "";
+    $age = isset($_POST["age"]) ? $_POST["age"] : "";
 
-// Verificar si se envió el formulario
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Crear conexión
-    $conn = new mysqli($server, $username, $password, $database);
-
-    // Verificar la conexión
-    if ($conn->connect_error) {
-        die("Error de conexión: " . $conn->connect_error);
-    }
-
-    // Obtener datos del formulario
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-    $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash de la contraseña
-
-    // Preparar la consulta SQL
-    $query = "INSERT INTO usuarios (nombre, apellido, username, password) VALUES ('$nombre', '$apellido', '$username', '$password')";
-
-    // Ejecutar la consulta
-    if ($conn->query($query) === TRUE) {
-        echo "Registro exitoso";
-    } else {
-        echo "Error en el registro: " . $conn->error;
-    }
-
-    // Cerrar la conexión
-    $conn->close();
+    // Display the received values
+    echo "Received data:\n";
+    echo "Name: $name\n";
+    echo "Age: $age\n";
+} else {
+    // If the request method is not POST, display an error
+    http_response_code(405); // Method Not Allowed
+    echo "Invalid request method. Only POST requests are allowed.";
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de Usuario</title>
-</head>
-<body>
-    <h2>Registro de Usuario</h2>
-    <form action="test.php" method="post">
-        <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre" required><br>
-
-        <label for="apellido">Apellido:</label>
-        <input type="text" name="apellido" required><br>
-
-        <label for="username">Nombre de Usuario:</label>
-        <input type="text" name="username" required><br>
-
-        <label for="password">Contraseña:</label>
-        <input type="password" name="password" required><br>
-
-        <input type="submit" value="Registrar">
-    </form>
-</body>
-</html>
