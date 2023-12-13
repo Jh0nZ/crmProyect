619029820bb15fcba92b0badcb57732307437916
jhon202107786 <202107786@est.umss.edu>
2023-12-13 11:00:22 -0400

    f

--- resources/include/sesion.php
+++ resources/include/sesion.php
@@ -2,7 +2,7 @@
 session_start();
 $response = ['success' => false];
 // Verifica si el usuario ha iniciado sesión
-if (isset($_SESSION['username'])) {
+if (isset($_SESSION["user_id"]) && isset($_SESSION["store_id"])) {
     $response['success'] = true;
 }
 echo json_encode($response);
