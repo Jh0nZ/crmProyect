463c46ffc613ee10d03d13eaea3d3157864ec0d7
jhon202107786 <202107786@est.umss.edu>
2023-12-13 09:25:17 -0400

    as

--- resources/include/obtenerFases.php
+++ resources/include/obtenerFases.php
@@ -11,7 +11,7 @@ if (isset($_SESSION["user_id"]) && isset($_SESSION["store_id"]) && isset($_SESSI
         echo json_encode(array("error" => "Error conexion base de datos"));
     }
 
-    $getFunnelSql = "SELECT f.name AS funnel_name, p.name AS phase_name
+    $getFunnelSql = "SELECT f.name AS funnel_name, p.name AS phase_name, p.phase_id
                      FROM funnel f
                      JOIN phase p ON f.funnel_id = p.funnel_id
                      WHERE f.store_id = $store_id";
@@ -23,12 +23,13 @@ if (isset($_SESSION["user_id"]) && isset($_SESSION["store_id"]) && isset($_SESSI
         while ($row = $result->fetch_assoc()) {
             $funnel_name = $row['funnel_name'];
             $phase_name = $row['phase_name'];
+            $phase_id = $row['phase_id'];
             
-            // Agregar el phase_name al array asociativo
+            // Agregar el phase_name y phase_id al array asociativo
             if (!isset($data[$funnel_name])) {
                 $data[$funnel_name] = array();
             }
-            $data[$funnel_name][] = $phase_name;
+            $data[$funnel_name][] = array("phase_name" => $phase_name, "phase_id" => $phase_id);
         }
         
         header('Content-Type: application/json');
